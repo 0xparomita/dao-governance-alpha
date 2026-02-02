7e9f271da9761dd2e694d583dc8395235b8f3962
Parmita <160781867+0xparomita@users.noreply.github.com>
2026-02-02 13:56:44 +0600

    Create GovToken.sol

---
+++ GovToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GovToken is ERC20, ERC20Votes, Ownable {
+    constructor() ERC20("GovToken", "GTK") ERC20Permit("GovToken") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+
+    // Required overrides for Solidity
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+}
